183873548065c7897314bd93462cfc8d5a7b0655
Rodrigo Santomauro <rodrigoolema@outlook.com>
2022-04-20 21:49:49 -0300

    Archivos basicos para terraform

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
 resource "aws_instance" "test-ec2" {
-  ami   = "ami-03ededff12e34e59e"
-  name  = "practico1"
-  
-  tags {
-    Name = "practico1"
+  ami           = "ami-03ededff12e34e59e"
+  instance_type = "t2.micro"
+  key_name      = "vockey"
+  vpc_security_group_ids = ["sg-011ed1a174751c1f4"]
+  tags = {
+    Name      = "practico1"
+    terraform = "True"
+
   }
 }
