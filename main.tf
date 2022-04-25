f9c9a2b89b1744004073bfd13c146623b38933ca
Rodrigo Santomauro <rodrigoolema@outlook.com>
2022-04-25 19:53:23 -0300

    first commit

--- main.tf
+++ main.tf
@@ -1,7 +1,35 @@
-provider "aws" {
-  region  = "us-east-1"
-  profile = "default"
+resource "aws_vpc" "Grupo7" {
+  cidr_block = "${var.cidr_block}.0.0/16"
 }
+
+resource "aws_internet_gateway" "Grupo7" {
+  // llama el id para crearlo o llamarlo del vpc
+ vpc_id =  aws_vpc.Grupo7.id
+}
+
+resource "aws_subnet" "Grupo7-az1" {
+  vpc_id = aws_vpc.Grupo7.id
+  cidr_block = "${var.cidr_block}.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_security_group" "Grupo7-SG" {
+  name = "Grupo7-SG"
+  vpc_id = aws_vpc.Grupo7.id
+  ingress = [ {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    description = "Grupo7"
+    from_port = 22
+    protocol = "-1"
+    to_port = 22
+  } ]
+  egress = [ {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    description = "Grupo7"
+    protocol = "-1"
+  } ]
+}
+
 resource "aws_instance" "test-ec2" {
   ami           = "ami-03ededff12e34e59e"
   instance_type = "t2.micro"
@@ -10,6 +38,5 @@ resource "aws_instance" "test-ec2" {
   tags = {
     Name      = "practico1"
     terraform = "True"
-
   }
 }
